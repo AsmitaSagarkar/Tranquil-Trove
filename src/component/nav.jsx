99299a3c150bb0748aa270d0f68e7742709b3da3
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-03-06 23:58:47 +0530

    navbar updated

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -17,21 +17,21 @@ export default function Nav(){
        <img className="w-40 h-36" src={Logo} alt="Tranquil TROVE LOADING ......" />
        </div>
 
-       <div className="hidden lg:flex lg:items-center lg:justify-between lg:text-2xl text-pink-500 font-[poppins] font-semibold">
+       <div className="hidden lg:flex lg:items-center lg:justify-between lg:text-2xl text-emerald-700 font-[poppins] font-semibold">
        <NavLink to ="/home">Home</NavLink>
        <NavLink to ="/about">About</NavLink>
        <NavLink to ="/contact">Contact</NavLink>
        <NavLink to ="/product">Product</NavLink>
        </div>
 
-       <div className="hidden lg:flex lg:items-center lg:justify-end lg:gap-14 mr-7 text-2xl text-colorBlack font-[poppins] font-semibold">
-       <NavLink to ="/login" className="bg-colorBlack  px-4 py-1 rounded-xl ">Sign in</NavLink>
+       <div className="hidden lg:flex lg:items-center lg:justify-end lg:gap-14 mr-7 text-2xl text-black font-[poppins] font-semibold">
+       <NavLink to ="/login" className="px-4 py-1 rounded-xl ">Sign in</NavLink>
        <NavLink to ="/search"><SearchIcon /></NavLink>
-       <NavLink to ="/cart" className="relative"><ShoppingCartIcon /><div className = "w-2 h-7 text-[20px] bg-pink-500 text-colorBlack rounded-full flex justify-center absolute top-0 right-0">0</div></NavLink>
+       <NavLink to ="/cart" className="relative"><ShoppingCartIcon /><div className = "w-2 h-7 text-[20px] bg-emerald-700 text-colorBlack rounded-full flex justify-center absolute top-0 right-0">0</div></NavLink>
        </div>
 
        <button className="lg:hidden flex items-center justify-end mr-5 z-50 text-2xl"> 
-       {isOpen ? <CancelIcon onClick = {toggle} className="text-pink-500 "  /> : <WidgetsIcon onClick ={toggle} />}
+       {isOpen ? <CancelIcon onClick = {toggle} className="text-emerald-700"  /> : <WidgetsIcon onClick ={toggle} className="text-emerald-700" />}
        
        </button>
 
@@ -39,14 +39,14 @@ export default function Nav(){
        {
         isOpen&& (
 
-            <div className="h-full justify-center items-center flex flex-col gap-6 text-2xl font-[poppins]  bg-colorBlack absolute w-full text-colorYellow font-bold">
-            <NavLink to ="/home">Home</NavLink>
-            <NavLink to ="/about">About</NavLink>
-            <NavLink to ="/contact">Contact</NavLink>
-            <NavLink to ="/product">Product</NavLink>
-            <NavLink to ="/login" className="bg-colorYellow text-colorBlack  px-4 py-1 rounded-xl ">Sign in</NavLink>
-            <NavLink to ="/search"><SearchIcon /></NavLink>
-            <NavLink to ="/cart"><ShoppingCartIcon /></NavLink>
+            <div className="h-full justify-center items-center flex flex-col gap-6 text-2xl font-[poppins]  bg-colorBlack absolute w-full text-emerald-700 font-bold">
+            <NavLink to ="/home" onClick={toggle}>Home</NavLink>
+            <NavLink to ="/about"onClick={toggle}>About</NavLink>
+            <NavLink to ="/contact"onClick={toggle}>Contact</NavLink>
+            <NavLink to ="/product"onClick={toggle}>Product</NavLink>
+            <NavLink to ="/login" className="bg-emerald-700 text-black  px-4 py-1 rounded-xl ">Sign in</NavLink>
+            <NavLink to ="/search"onClick={toggle}><SearchIcon /></NavLink>
+            <NavLink to ="/cart"onClick={toggle}><ShoppingCartIcon /></NavLink>
             
             
             </div>
