d0334e4fa6d5cfdb37577069e191f031b0ba000d
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-03-11 23:59:17 +0530

    api added

--- src/component/context/productcontext.jsx
+++ src/component/context/productcontext.jsx
@@ -1,18 +1,24 @@
 //here we have to create a context for the product. 
 //Step to create a context : create a context,present a provider,present a consumer using useContext hook
-
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
 import Axios from 'axios';
-
 
 const AppContext = createContext(); 
 //console.log(AppContext);
 
-const API = "https://api.pujakaitem.com/api/products";
+const API = "https://dummyjson.com/products";
 
 
+const initialState = {
 
+    isLoading :false,
+    isError : false,
+    product : [],
+    pRating : []
+}
 const AppProvider = ({children}) =>{
+
+    const [state,dispatch] = useReducer(reducer,initialState);
 
     const getApi = async (url) =>{
         const res = await Axios.get(url);
