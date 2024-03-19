1a43367133fec7863f411316ee47ee453e465d2c
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-03-19 18:56:08 +0530

    usereducer implemented

--- src/component/reducer/productReducer.jsx
+++ src/component/reducer/productReducer.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-
 const productReducer = (state,action) => {
   switch(action.type){
     case "LOADING":
       return {...state,isLoading:true}
     case "SET_PRODUCT":
-        const rating = action.payload.filter((item)=> {return item.rating > 4});
+        const rating = action.payload.products.filter((item)=> {return item.rating > 4});
+        console.log(action.payload);
         return {...state,isLoading:false,product:action.payload,pRating:rating}
     case "ERROR":
         return {...state,isError:true}
   }
 }
 
-export default productReducer
+export default productReducer;
